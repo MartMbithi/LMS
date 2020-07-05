bb9d2dda6014cd03996afeef6cabf7f3b27410bb
MartMbithi <martdevelopers254@gmail.com>
2020-07-05 11:12:00 +0300

    Index File To Redirect To Main System Core Files

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS'])) {
+    $uri = 'https://';
+} else {
+    $uri = 'http://';
+}
+$uri .= $_SERVER['HTTP_HOST'];
+header('Location: ' . $uri . '/LMS/public/');
+exit;
+?>
+
+Something wrong with ya installation
