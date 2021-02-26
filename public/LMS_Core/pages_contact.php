5ac8d743530e14d869f95b5d86e71f651114407c
MartMbithi <martdevelopers254@gmail.com>
2021-02-26 10:12:00 +0300

    A Little Codebase House Keeping

--- public/LMS_Core/pages_contact.php
+++ public/LMS_Core/pages_contact.php
@@ -1,132 +1,131 @@
 <?php
-  //session_start();
-  include('admin/dist/inc/config.php');
-  //include('dist/inc/checklogin.php');
-  //check_login();
-  //$a_id=$_SESSION['a_id'];
+//session_start();
+include('admin/dist/inc/config.php');
+//include('dist/inc/checklogin.php');
+//check_login();
+//$a_id=$_SESSION['a_id'];
 
-  //register a course category
-  if(isset($_POST['send_messange']))
-  {
-	  $name = $_POST['name'];
-	  $email =$_POST['email'];
-	  $subject =$_POST['subject'];
-	  $msg = $_POST['msg'];
-      //sql to insert captured values
-      $query="INSERT INTO lms_messanges (name, email, subject, msg) VALUES (?,?,?,?)";
-      $stmt = $mysqli->prepare($query);
-      $rc=$stmt->bind_param('ssss', $name, $email, $subject, $msg);
-      $stmt->execute();
+//register a course category
+if (isset($_POST['send_messange'])) {
+	$name = $_POST['name'];
+	$email = $_POST['email'];
+	$subject = $_POST['subject'];
+	$msg = $_POST['msg'];
+	//sql to insert captured values
+	$query = "INSERT INTO lms_messanges (name, email, subject, msg) VALUES (?,?,?,?)";
+	$stmt = $mysqli->prepare($query);
+	$rc = $stmt->bind_param('ssss', $name, $email, $subject, $msg);
+	$stmt->execute();
 
-      if($stmt)
-      {
-                $success = "Messange Send";
-                
-                //echo "<script>toastr.success('Have Fun')</script>";
-      }
-      else {
-        $err = "Please Try Again Or Try Later";
-      }
-      
-      
-  }
+	if ($stmt) {
+		$success = "Messange Send";
+
+		//echo "<script>toastr.success('Have Fun')</script>";
+	} else {
+		$err = "Please Try Again Or Try Later";
+	}
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
-	<?php include("inc/head.php");?>
-	<body>
+<?php include("inc/head.php"); ?>
 
-		<!-- Header -->
-			<?php include("inc/header.php");?>
-		<!-- /Header -->
+<body>
 
-		<!-- Hero-area -->
-		<div class="hero-area section">
+	<!-- Header -->
+	<?php include("inc/header.php"); ?>
+	<!-- /Header -->
 
-			<!-- Backgound Image -->
-			<div class="bg-image bg-parallax overlay" style="background-image:url(./img/page-background.jpg)"></div>
-			<!-- /Backgound Image -->
+	<!-- Hero-area -->
+	<div class="hero-area section">
 
-			<div class="container">
-				<div class="row">
-					<div class="col-md-10 col-md-offset-1 text-center">
-						<ul class="hero-area-tree">
-							<li><a href="index.php">Home</a></li>
-							<li>Contact</li>
-						</ul>
-						<h1 class="white-text">Get In Touch</h1>
+		<!-- Backgound Image -->
+		<div class="bg-image bg-parallax overlay" style="background-image:url(./img/page-background.jpg)"></div>
+		<!-- /Backgound Image -->
 
-					</div>
-				</div>
-			</div>
-
-		</div>
-		<!-- /Hero-area -->
-
-		<!-- Contact -->
-		<div id="contact" class="section">
-
-			<!-- container -->
-			<div class="container">
-
-				<!-- row -->
-				<div class="row">
-
-					<!-- contact form -->
-					<div class="col-md-6">
-						<div class="contact-form">
-							<h4>Send A Message</h4>
-							<form method = "POST">
-								<input class="input" required type="text" name="name" placeholder="Name">
-								<input class="input" required type="email" name="email" placeholder="Email">
-								<input class="input" required type="text" name="subject" placeholder="Subject">
-								<textarea class="input" required name="msg" placeholder="Enter your Message"></textarea>
-								<input type="submit" name="send_messange" class="main-button icon-button pull-right" value="Send Message">
-							</form>
-						</div>
-					</div>
-					<!-- /contact form -->
-
-					<!-- contact information -->
-					<div class="col-md-5 col-md-offset-1">
-						<h4>Contact Information</h4>
-						<ul class="contact-details">
-							<li><i class="fa fa-envelope"></i>mail@lms.com</li>
-							<li><i class="fa fa-phone"></i>+254740847563</li>
-							<li><i class="fa fa-map-marker"></i>127001 LocalHost</li>
-						</ul>
-
-						<!-- contact map -->
-						<div id="contact-map"></div>
-						<!-- /contact map -->
-
-					</div>
-					<!-- contact information -->
+		<div class="container">
+			<div class="row">
+				<div class="col-md-10 col-md-offset-1 text-center">
+					<ul class="hero-area-tree">
+						<li><a href="index.php">Home</a></li>
+						<li>Contact</li>
+					</ul>
+					<h1 class="white-text">Get In Touch</h1>
 
 				</div>
-				<!-- /row -->
+			</div>
+		</div>
+
+	</div>
+	<!-- /Hero-area -->
+
+	<!-- Contact -->
+	<div id="contact" class="section">
+
+		<!-- container -->
+		<div class="container">
+
+			<!-- row -->
+			<div class="row">
+
+				<!-- contact form -->
+				<div class="col-md-6">
+					<div class="contact-form">
+						<h4>Send A Message</h4>
+						<form method="POST">
+							<input class="input" required type="text" name="name" placeholder="Name">
+							<input class="input" required type="email" name="email" placeholder="Email">
+							<input class="input" required type="text" name="subject" placeholder="Subject">
+							<textarea class="input" required name="msg" placeholder="Enter your Message"></textarea>
+							<input type="submit" name="send_messange" class="main-button icon-button pull-right" value="Send Message">
+						</form>
+					</div>
+				</div>
+				<!-- /contact form -->
+
+				<!-- contact information -->
+				<div class="col-md-5 col-md-offset-1">
+					<h4>Contact Information</h4>
+					<ul class="contact-details">
+						<li><i class="fa fa-envelope"></i>mail@lms.com</li>
+						<li><i class="fa fa-phone"></i>+254740847563</li>
+						<li><i class="fa fa-map-marker"></i>127001 LocalHost</li>
+					</ul>
+
+					<!-- contact map -->
+					<div id="contact-map"></div>
+					<!-- /contact map -->
+
+				</div>
+				<!-- contact information -->
 
 			</div>
-			<!-- /container -->
+			<!-- /row -->
 
 		</div>
-		<!-- /Contact -->
+		<!-- /container -->
 
-		<!-- Footer -->
-			<?php include("inc/footer.php");?>
-		<!-- /Footer -->
+	</div>
+	<!-- /Contact -->
 
-		<!-- preloader -->
-		<div id='preloader'><div class='preloader'></div></div>
-		<!-- /preloader -->
+	<!-- Footer -->
+	<?php include("inc/footer.php"); ?>
+	<!-- /Footer -->
+
+	<!-- preloader -->
+	<div id='preloader'>
+		<div class='preloader'></div>
+	</div>
+	<!-- /preloader -->
 
 
-		<!-- jQuery Plugins -->
-		<script type="text/javascript" src="js/jquery.min.js"></script>
-		<script type="text/javascript" src="js/bootstrap.min.js"></script>
-		<script src="https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=true"></script>
-		<script type="text/javascript" src="js/google-map.js"></script>
-		<script type="text/javascript" src="js/main.js"></script>
+	<!-- jQuery Plugins -->
+	<script type="text/javascript" src="js/jquery.min.js"></script>
+	<script type="text/javascript" src="js/bootstrap.min.js"></script>
+	<script src="https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=true"></script>
+	<script type="text/javascript" src="js/google-map.js"></script>
+	<script type="text/javascript" src="js/main.js"></script>
 
-	</body>
+</body>
+
 </html>
--- public/LMS_Core/pages_course_details.php
+++ public/LMS_Core/pages_course_details.php
@@ -1,103 +1,101 @@
-
-<?php 
- include("admin/dist/inc/config.php");
- //load configuration file
+<?php
+include("admin/dist/inc/config.php");
+//load configuration file
 
 //get all courses
 $c_id = $_GET['c_id'];
-$ret="SELECT  * FROM  lms_course WHERE c_id = ? ";
-$stmt= $mysqli->prepare($ret) ;
-$stmt->bind_param('i',$c_id);
-$stmt->execute() ;//ok
-$res=$stmt->get_result();
+$ret = "SELECT  * FROM  lms_course WHERE c_id = ? ";
+$stmt = $mysqli->prepare($ret);
+$stmt->bind_param('i', $c_id);
+$stmt->execute(); //ok
+$res = $stmt->get_result();
 //$cnt=1;
-while($row=$res->fetch_object())
-{
-    ?>
+while ($row = $res->fetch_object()) {
+?>
     <!DOCTYPE html>
     <html lang="en">
-        <?php include("inc/head.php");?>
-        <body>
+    <?php include("inc/head.php"); ?>
 
-            <!-- Header -->
-            <?php include("inc/header.php");?>
-            <!-- /Header -->
+    <body>
 
-            <!-- Hero-area -->
-            <div class="hero-area section">
+        <!-- Header -->
+        <?php include("inc/header.php"); ?>
+        <!-- /Header -->
 
-                <!-- Backgound Image -->
-                <div class="bg-image bg-parallax overlay" style="background-image:url(./img/blog-post-background.jpg)"></div>
-                <!-- /Backgound Image -->
+        <!-- Hero-area -->
+        <div class="hero-area section">
 
-                <div class="container">
-                    <div class="row">
-                        <div class="col-md-10 col-md-offset-1 text-center">
-                            <ul class="hero-area-tree">
-                                <li><a href="index.php">Home</a></li>
-                                <li><a href=""><?php echo $row->c_category;?></a></li>
-                                <li><?php echo $row->c_name;?></li>
-                            </ul>
-                            <h1 class="white-text"><?php echo $row->c_name;?></h1>
-                            
-                        </div>
+            <!-- Backgound Image -->
+            <div class="bg-image bg-parallax overlay" style="background-image:url(./img/blog-post-background.jpg)"></div>
+            <!-- /Backgound Image -->
+
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-10 col-md-offset-1 text-center">
+                        <ul class="hero-area-tree">
+                            <li><a href="index.php">Home</a></li>
+                            <li><a href=""><?php echo $row->c_category; ?></a></li>
+                            <li><?php echo $row->c_name; ?></li>
+                        </ul>
+                        <h1 class="white-text"><?php echo $row->c_name; ?></h1>
+
                     </div>
                 </div>
-
             </div>
-            <!-- /Hero-area -->
 
-            <!-- Blog -->
-            <div id="blog" class="section">
+        </div>
+        <!-- /Hero-area -->
 
-                <!-- container -->
-                <div class="container">
+        <!-- Blog -->
+        <div id="blog" class="section">
 
-                    <!-- row -->
-                    <div class="row">
+            <!-- container -->
+            <div class="container">
 
-                        <!-- main blog -->
-                        <div id="main" class="col-md-12">
+                <!-- row -->
+                <div class="row">
 
-                            <!-- blog post -->
-                            <div class="blog-post">
-                                <?php echo $row->c_desc;?>
-                                
-                            </div>
-                            <!-- /blog post -->
+                    <!-- main blog -->
+                    <div id="main" class="col-md-12">
 
-                            
+                        <!-- blog post -->
+                        <div class="blog-post">
+                            <?php echo $row->c_desc; ?>
 
-                            <!-- blog comments -->
-                            <div class="blog-comments">
-                                <h3>Posted Revision Questions</h3>
-                                <?php
-                                    //get questions
-                                    $c_id = $_GET['c_id'];
-                                    $ret="SELECT  * FROM  lms_questions WHERE c_id = ? ";
-                                    $stmt= $mysqli->prepare($ret) ;
-                                    $stmt->bind_param('i',$c_id);
-                                    $stmt->execute() ;//ok
-                                    $res=$stmt->get_result();
-                                    //$cnt=1;
-                                    while($row=$res->fetch_object())
-                                    {   
-                                ?>
-                                    <!-- single comment -->
-                                    <div class="media">
-                                        <div class="media-left">
-                                            <img src="./img/avatar.png" alt="">
-                                        </div>
-                                        <div class="media-body">
-                                            <h4 class="media-heading"><?php echo $row->c_name;?></h4>
-                                            <?php echo $row->q_details;?>
-                                        </div>
+                        </div>
+                        <!-- /blog post -->
+
+
+
+                        <!-- blog comments -->
+                        <div class="blog-comments">
+                            <h3>Posted Revision Questions</h3>
+                            <?php
+                            //get questions
+                            $c_id = $_GET['c_id'];
+                            $ret = "SELECT  * FROM  lms_questions WHERE c_id = ? ";
+                            $stmt = $mysqli->prepare($ret);
+                            $stmt->bind_param('i', $c_id);
+                            $stmt->execute(); //ok
+                            $res = $stmt->get_result();
+                            //$cnt=1;
+                            while ($row = $res->fetch_object()) {
+                            ?>
+                                <!-- single comment -->
+                                <div class="media">
+                                    <div class="media-left">
+                                        <img src="./img/avatar.png" alt="">
+                                    </div>
+                                    <div class="media-body">
+                                        <h4 class="media-heading"><?php echo $row->c_name; ?></h4>
+                                        <?php echo $row->q_details; ?>
+                                    </div>
 
                                     <!-- /single comment -->
 
-                                <?php }?>
-                            </div>
-                           
+                                <?php } ?>
+                                </div>
+
                         </div>
                         <!-- /main blog -->
 
@@ -112,11 +110,13 @@ while($row=$res->fetch_object())
             <!-- /Blog -->
 
             <!-- Footer -->
-            <?php include("inc/footer.php");?>
+            <?php include("inc/footer.php"); ?>
             <!-- /Footer -->
 
             <!-- preloader -->
-            <div id='preloader'><div class='preloader'></div></div>
+            <div id='preloader'>
+                <div class='preloader'></div>
+            </div>
             <!-- /preloader -->
 
 
@@ -125,6 +125,7 @@ while($row=$res->fetch_object())
             <script type="text/javascript" src="js/bootstrap.min.js"></script>
             <script type="text/javascript" src="js/main.js"></script>
 
-        </body>
+    </body>
+
     </html>
-<?php }?>
+<?php } ?>
