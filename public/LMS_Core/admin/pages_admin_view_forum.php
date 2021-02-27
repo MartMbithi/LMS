4b865306e438218a6d1a04b5937f10d2da81f81b
MartMbithi <martdevelopers254@gmail.com>
2021-02-27 10:12:00 +0300

    A Little Codebase House Keeping

--- public/LMS_Core/admin/pages_admin_view_forum.php
+++ public/LMS_Core/admin/pages_admin_view_forum.php
@@ -1,10 +1,10 @@
 <?php
-  session_start();
-  include('dist/inc/config.php');
-  include('dist/inc/checklogin.php');
-  check_login();
-  $a_id=$_SESSION['a_id'];
-  /*
+session_start();
+include('dist/inc/config.php');
+include('dist/inc/checklogin.php');
+check_login();
+$a_id = $_SESSION['a_id'];
+/*
   //register a new student
   if(isset($_POST['add_student']))
   {
@@ -45,30 +45,29 @@
 <html dir="ltr" lang="en">
 
 <!--Head-->
-<?php include("dist/inc/head.php");?>
+<?php include("dist/inc/head.php"); ?>
 <!-- ./Head -->
 
 <body onload=display_ct();>
     <!-- ============================================================== -->
     <!-- Preloader - style you can find in spinners.css -->
     <!-- ============================================================== -->
-    
+
     <!-- ============================================================== -->
     <!-- Main wrapper - style you can find in pages.scss -->
     <!-- ============================================================== -->
-    <div id="main-wrapper" data-theme="light" data-layout="vertical" data-navbarbg="skin6" data-sidebartype="full"
-        data-sidebar-position="fixed" data-header-position="fixed" data-boxed-layout="full">
+    <div id="main-wrapper" data-theme="light" data-layout="vertical" data-navbarbg="skin6" data-sidebartype="full" data-sidebar-position="fixed" data-header-position="fixed" data-boxed-layout="full">
         <!-- ============================================================== -->
         <!-- Topbar header - style you can find in pages.scss -->
         <!-- ============================================================== -->
-            <?php include("dist/inc/header.php");?>
+        <?php include("dist/inc/header.php"); ?>
         <!-- ============================================================== -->
         <!-- End Topbar header -->
         <!-- ============================================================== -->
         <!-- ============================================================== -->
         <!-- Left Sidebar - style you can find in sidebar.scss  -->
         <!-- ============================================================== -->
-            <?php include("dist/inc/sidebar.php");?>
+        <?php include("dist/inc/sidebar.php"); ?>
         <!-- ============================================================== -->
         <!-- End Left Sidebar - style you can find in sidebar.scss  -->
         <!-- ============================================================== -->
@@ -83,82 +82,69 @@
                 <div class="row">
                     <div class="col-7 align-self-center">
                         <?php
-                            $a_id = $_SESSION['a_id'];
-                            $ret="SELECT  * FROM  lms_admin  WHERE a_id=?";
-                            $stmt= $mysqli->prepare($ret) ;
-                            $stmt->bind_param('i',$a_id);
-                            $stmt->execute() ;//ok
-                            $res=$stmt->get_result();
-                            //$cnt=1;
-                            while($row=$res->fetch_object())
-                            {
-                                // time function to get day zones ie morning, noon, and night.
-                                $t = date("H");
+                        $a_id = $_SESSION['a_id'];
+                        $ret = "SELECT  * FROM  lms_admin  WHERE a_id=?";
+                        $stmt = $mysqli->prepare($ret);
+                        $stmt->bind_param('i', $a_id);
+                        $stmt->execute(); //ok
+                        $res = $stmt->get_result();
+                        //$cnt=1;
+                        while ($row = $res->fetch_object()) {
+                            // time function to get day zones ie morning, noon, and night.
+                            $t = date("H");
 
-                                if ($t < "10")
-                                 {
-                                    $d_time = "Good Morning";
+                            if ($t < "10") {
+                                $d_time = "Good Morning";
+                            } elseif ($t < "15") {
 
-                                    }
+                                $d_time =  "Good Afternoon";
+                            } elseif ($t < "20") {
 
-                                     elseif ($t < "15")
-                                      {
+                                $d_time =  "Good Evening";
+                            } else {
 
-                                      $d_time =  "Good Afternoon";
-
-                                     } 
-
-                                        elseif ($t < "20")
-                                        {
-
-                                        $d_time =  "Good Evening";
-
-                                        } 
-                                        else {
-
-                                            $d_time = "Good Night";
-                                }
+                                $d_time = "Good Night";
+                            }
                         ?>
-                        <h3 class="page-title text-truncate text-dark font-weight-medium mb-1"><?php echo $d_time;?> <?php echo $row->a_uname;?></h3>
-                        <?php }?>
+                            <h3 class="page-title text-truncate text-dark font-weight-medium mb-1"><?php echo $d_time; ?> <?php echo $row->a_uname; ?></h3>
+                        <?php } ?>
                         <!--Get Details of single Forum Topic-->
                         <?php
-                            $f_id = $_GET['f_id'];
-                            $ret="SELECT  * FROM lms_forum  WHERE f_id=?";
-                            $stmt= $mysqli->prepare($ret) ;
-                            $stmt->bind_param('i',$f_id);
-                            $stmt->execute() ;//ok
-                            $res=$stmt->get_result();
-                            //$cnt=1;
-                            while($row=$res->fetch_object())
-                            {
-                              
+                        $f_id = $_GET['f_id'];
+                        $ret = "SELECT  * FROM lms_forum  WHERE f_id=?";
+                        $stmt = $mysqli->prepare($ret);
+                        $stmt->bind_param('i', $f_id);
+                        $stmt->execute(); //ok
+                        $res = $stmt->get_result();
+                        //$cnt=1;
+                        while ($row = $res->fetch_object()) {
+
                         ?>
-                        <div class="d-flex align-items-center">
-                            <nav aria-label="breadcrumb">
-                                 <ol class="breadcrumb m-0 p-0">
-                                    <li class="breadcrumb-item"><a href="pages_admin_dashboard.php">Dashboard</a>
-                                    </li>
-                                    <li class="breadcrumb-item"><a href="pages_admin_manage_forum.php">Forum</a>
-                                    </li>
-                                    <li class="breadcrumb-item"><a href="pages_admin_manage_forum.php">Manage Discussion</a>
-                                    </li>
-                                    <li class="breadcrumb-item"><a href=""><?php echo $row->s_unit_name;?> Discussion</a>
-                                    </li>
-                                    
-                                </ol>
-                            </nav>
-                        </div>
+                            <div class="d-flex align-items-center">
+                                <nav aria-label="breadcrumb">
+                                    <ol class="breadcrumb m-0 p-0">
+                                        <li class="breadcrumb-item"><a href="pages_admin_dashboard.php">Dashboard</a>
+                                        </li>
+                                        <li class="breadcrumb-item"><a href="pages_admin_manage_forum.php">Forum</a>
+                                        </li>
+                                        <li class="breadcrumb-item"><a href="pages_admin_manage_forum.php">Manage Discussion</a>
+                                        </li>
+                                        <li class="breadcrumb-item"><a href=""><?php echo $row->s_unit_name; ?> Discussion</a>
+                                        </li>
+
+                                    </ol>
+                                </nav>
+                            </div>
                     </div>
                     <div class="col-5 align-self-center">
                         <div class="customize-input float-right">
                             <select class="custom-select custom-select-set form-control bg-white border-0 custom-shadow custom-radius">
                                 <option selected id="ct"></option>
-                                
+
                             </select>
                         </div>
                     </div>
-                    
+
                 </div>
             </div>
             <!-- ============================================================== -->
@@ -172,28 +158,27 @@
 
                     <div class="col-lg-12 col-md-6">
                         <div class="card-header">
-                            <?php echo $row->s_unit_code;?> <?php echo $row->s_unit_name;?> 
+                            <?php echo $row->s_unit_code; ?> <?php echo $row->s_unit_name; ?>
                         </div>
                         <hr>
-                       
-                        <table  class="table table-striped table-bordered display no-wrap" 
-                            style="width:100%">
+
+                        <table class="table table-striped table-bordered display no-wrap" style="width:100%">
                             <thead>
                                 <tr>
                                     <th>Discussion Questions</th>
-                                    
+
                                 </tr>
                             </thead>
                             <tbody>
                                 <tr>
-                                    <td><?php echo $row->f_topic;?></td>
+                                    <td><?php echo $row->f_topic; ?></td>
                                 </tr>
                             </tbody>
                         </table>
-                            
-                            <!-- Card -->
+
+                        <!-- Card -->
                     </div>
-                    <?php }?>
+                <?php } ?>
                 </div>
                 <!-- *************************************************************** -->
             </div>
@@ -203,7 +188,7 @@
             <!-- ============================================================== -->
             <!-- footer -->
             <!-- ============================================================== -->
-                 <?php include("dist/inc/footer.php");?>
+            <?php include("dist/inc/footer.php"); ?>
             <!-- ============================================================== -->
             <!-- End footer -->
             <!-- ============================================================== -->
@@ -238,7 +223,7 @@
     <script src="assets/extra-libs/jvector/jquery-jvectormap-2.0.2.min.js"></script>
     <script src="assets/extra-libs/jvector/jquery-jvectormap-world-mill-en.js"></script>
     <script src="dist/js/pages/dashboards/dashboard1.min.js"></script>
-    
+
     <!--This page plugins -->
     <script src="assets/extra-libs/datatables.net/js/jquery.dataTables.min.js"></script>
     <script src="dist/js/pages/datatable/datatable-basic.init.js"></script>
