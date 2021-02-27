a6fac154012a5bf694528ce13db54f0d10eb799c
MartMbithi <martdevelopers254@gmail.com>
2021-02-27 10:12:00 +0300

    A Little Codebase House Keeping

--- public/LMS_Core/admin/pages_admin_manage_certificates.php
+++ public/LMS_Core/admin/pages_admin_manage_certificates.php
@@ -1,58 +1,53 @@
 <?php
-  session_start();
-  include('dist/inc/config.php');
-  include('dist/inc/checklogin.php');
-  check_login();
-  $a_id=$_SESSION['a_id'];
-  //hold logged in user session.
-  //delete certificate primary details
-  if(isset($_GET['delete_cert']))
-  {
-        $id=intval($_GET['delete_cert']);
-        $adn="DELETE FROM lms_certs WHERE cert_id = ?";
-        $stmt= $mysqli->prepare($adn);
-        $stmt->bind_param('i',$id);
-        $stmt->execute();
-        $stmt->close();	 
-  
-          if($stmt)
-          {
-            $success = "Record Deleted";
-          }
-            else
-            {
-                $err = "Try Again Later";
-            }
+session_start();
+include('dist/inc/config.php');
+include('dist/inc/checklogin.php');
+check_login();
+$a_id = $_SESSION['a_id'];
+//hold logged in user session.
+//delete certificate primary details
+if (isset($_GET['delete_cert'])) {
+    $id = intval($_GET['delete_cert']);
+    $adn = "DELETE FROM lms_certs WHERE cert_id = ?";
+    $stmt = $mysqli->prepare($adn);
+    $stmt->bind_param('i', $id);
+    $stmt->execute();
+    $stmt->close();
+
+    if ($stmt) {
+        $success = "Record Deleted";
+    } else {
+        $err = "Try Again Later";
     }
+}
 ?>
 <!DOCTYPE html>
 <html dir="ltr" lang="en">
 
 <!--Head-->
-<?php include("dist/inc/head.php");?>
+<?php include("dist/inc/head.php"); ?>
 <!-- ./Head -->
 
 <body onload=display_ct();>
     <!-- ============================================================== -->
     <!-- Preloader - style you can find in spinners.css -->
     <!-- ============================================================== -->
-    
+
     <!-- ============================================================== -->
     <!-- Main wrapper - style you can find in pages.scss -->
     <!-- ============================================================== -->
-    <div id="main-wrapper" data-theme="light" data-layout="vertical" data-navbarbg="skin6" data-sidebartype="full"
-        data-sidebar-position="fixed" data-header-position="fixed" data-boxed-layout="full">
+    <div id="main-wrapper" data-theme="light" data-layout="vertical" data-navbarbg="skin6" data-sidebartype="full" data-sidebar-position="fixed" data-header-position="fixed" data-boxed-layout="full">
         <!-- ============================================================== -->
         <!-- Topbar header - style you can find in pages.scss -->
         <!-- ============================================================== -->
-            <?php include("dist/inc/header.php");?>
+        <?php include("dist/inc/header.php"); ?>
         <!-- ============================================================== -->
         <!-- End Topbar header -->
         <!-- ============================================================== -->
         <!-- ============================================================== -->
         <!-- Left Sidebar - style you can find in sidebar.scss  -->
         <!-- ============================================================== -->
-            <?php include("dist/inc/sidebar.php");?>
+        <?php include("dist/inc/sidebar.php"); ?>
         <!-- ============================================================== -->
         <!-- End Left Sidebar - style you can find in sidebar.scss  -->
         <!-- ============================================================== -->
@@ -66,45 +61,33 @@
             <div class="page-breadcrumb">
                 <div class="row">
                     <div class="col-7 align-self-center">
-                    <?php
-                            $a_id = $_SESSION['a_id'];
-                            $ret="SELECT  * FROM  lms_admin  WHERE a_id=?";
-                            $stmt= $mysqli->prepare($ret) ;
-                            $stmt->bind_param('i',$a_id);
-                            $stmt->execute() ;//ok
-                            $res=$stmt->get_result();
-                            //$cnt=1;
-                            while($row=$res->fetch_object())
-                            {
-                                // time function to get day zones ie morning, noon, and night.
-                                $t = date("H");
+                        <?php
+                        $a_id = $_SESSION['a_id'];
+                        $ret = "SELECT  * FROM  lms_admin  WHERE a_id=?";
+                        $stmt = $mysqli->prepare($ret);
+                        $stmt->bind_param('i', $a_id);
+                        $stmt->execute(); //ok
+                        $res = $stmt->get_result();
+                        //$cnt=1;
+                        while ($row = $res->fetch_object()) {
+                            // time function to get day zones ie morning, noon, and night.
+                            $t = date("H");
 
-                                if ($t < "10")
-                                 {
-                                    $d_time = "Good Morning";
+                            if ($t < "10") {
+                                $d_time = "Good Morning";
+                            } elseif ($t < "15") {
 
-                                    }
+                                $d_time =  "Good Afternoon";
+                            } elseif ($t < "20") {
 
-                                     elseif ($t < "15")
-                                      {
+                                $d_time =  "Good Evening";
+                            } else {
 
-                                      $d_time =  "Good Afternoon";
-
-                                     } 
-
-                                        elseif ($t < "20")
-                                        {
-
-                                        $d_time =  "Good Evening";
-
-                                        } 
-                                        else {
-
-                                            $d_time = "Good Night";
-                                }
+                                $d_time = "Good Night";
+                            }
                         ?>
-                        <h3 class="page-title text-truncate text-dark font-weight-medium mb-1"><?php echo $d_time;?> <?php echo $row->a_uname;?></h3>
-                        <?php }?>
+                            <h3 class="page-title text-truncate text-dark font-weight-medium mb-1"><?php echo $d_time; ?> <?php echo $row->a_uname; ?></h3>
+                        <?php } ?>
                         <div class="d-flex align-items-center">
                             <nav aria-label="breadcrumb">
                                 <ol class="breadcrumb m-0 p-0">
@@ -114,7 +97,7 @@
                                     </li>
                                     <li class="breadcrumb-item"><a href="pages_admin_manage_certificates.php">Manage</a>
                                     </li>
-                                    
+
                                 </ol>
                             </nav>
                         </div>
@@ -123,7 +106,7 @@
                         <div class="customize-input float-right">
                             <select class="custom-select custom-select-set form-control bg-white border-0 custom-shadow custom-radius">
                                 <option selected id="ct"></option>
-                                
+
                             </select>
                         </div>
                     </div>
@@ -136,15 +119,14 @@
             <!-- Container fluid  -->
             <!-- ============================================================== -->
             <div class="container-fluid">
-                
+
                 <div class="row">
                     <div class="col-lg-12">
                         <div class="card">
                             <div class="card-body">
                                 <h4 class="card-title">Primary Certificate Records</h4>
                                 <div class="table-responsive">
-                                    <table id="default_order" class="table table-striped table-bordered display"
-                                        style="width:100%">
+                                    <table id="default_order" class="table table-striped table-bordered display" style="width:100%">
                                         <thead>
                                             <tr>
                                                 <th>Unit Code</th>
@@ -156,41 +138,38 @@
                                             </tr>
                                         </thead>
                                         <tbody>
-                                        <?php
+                                            <?php
                                             //Student certificates
-                                            $ret="SELECT  * FROM  lms_certs";
-                                            $stmt= $mysqli->prepare($ret) ;
+                                            $ret = "SELECT  * FROM  lms_certs";
+                                            $stmt = $mysqli->prepare($ret);
                                             //$stmt->bind_param('i',$l_id);
-                                            $stmt->execute() ;//ok
-                                            $res=$stmt->get_result();
-                                            $cnt=1;
-                                            while($row=$res->fetch_object())
-                                            {
-                                                $mysqlDateTime = $row->date_generated;//trim timestamp to DD/MM/YYYY formart
-                                                
-                                        ?>
-                                            <tr>
-                                                <td><?php echo $row->s_unit_code;?></td>
-                                                <td><?php echo $row->s_unit_name;?></td>
-                                                <td><?php echo $row->i_name;?></td>
-                                                <td><?php echo $row->s_name;?></td>
-                                                <td><?php echo date("d M Y", strtotime($mysqlDateTime));?></td>
-                                                <td>
-                                                    
-                                                    <a class="badge badge-success" 
-                                                         href="pages_admin_generate_certificate.php?cert_id=<?php echo $row->cert_id;?>">
-                                                         <i class=" fas fa-check"></i> <i class="  fas fa-certificate"></i>
-                                                             Generate Certificate
-                                                    </a>
-                                                    <a class="badge badge-danger" 
-                                                         href="pages_admin_manage_certificates.php?delete_cert=<?php echo $row->cert_id;?>">
-                                                         <i class="fas fa-trash"></i> <i class=" fas fa-pallet"></i>
-                                                            Delete Record
-                                                    </a>
-                                                </td>
-                                            </tr>
+                                            $stmt->execute(); //ok
+                                            $res = $stmt->get_result();
+                                            $cnt = 1;
+                                            while ($row = $res->fetch_object()) {
+                                                $mysqlDateTime = $row->date_generated; //trim timestamp to DD/MM/YYYY formart
 
-                                            <?php }?>    
+                                            ?>
+                                                <tr>
+                                                    <td><?php echo $row->s_unit_code; ?></td>
+                                                    <td><?php echo $row->s_unit_name; ?></td>
+                                                    <td><?php echo $row->i_name; ?></td>
+                                                    <td><?php echo $row->s_name; ?></td>
+                                                    <td><?php echo date("d M Y", strtotime($mysqlDateTime)); ?></td>
+                                                    <td>
+
+                                                        <a class="badge badge-success" href="pages_admin_generate_certificate.php?cert_id=<?php echo $row->cert_id; ?>">
+                                                            <i class=" fas fa-check"></i> <i class="  fas fa-certificate"></i>
+                                                            Generate Certificate
+                                                        </a>
+                                                        <a class="badge badge-danger" href="pages_admin_manage_certificates.php?delete_cert=<?php echo $row->cert_id; ?>">
+                                                            <i class="fas fa-trash"></i> <i class=" fas fa-pallet"></i>
+                                                            Delete Record
+                                                        </a>
+                                                    </td>
+                                                </tr>
+
+                                            <?php } ?>
 
                                         </tbody>
                                     </table>
@@ -198,9 +177,9 @@
                             </div>
                         </div>
                     </div>
-                       
+
                 </div>
-            
+
                 <!-- *************************************************************** -->
             </div>
             <!-- ============================================================== -->
@@ -209,7 +188,7 @@
             <!-- ============================================================== -->
             <!-- footer -->
             <!-- ============================================================== -->
-                 <?php include("dist/inc/footer.php");?>
+            <?php include("dist/inc/footer.php"); ?>
             <!-- ============================================================== -->
             <!-- End footer -->
             <!-- ============================================================== -->
@@ -244,7 +223,7 @@
     <script src="assets/extra-libs/jvector/jquery-jvectormap-2.0.2.min.js"></script>
     <script src="assets/extra-libs/jvector/jquery-jvectormap-world-mill-en.js"></script>
     <script src="dist/js/pages/dashboards/dashboard1.min.js"></script>
-    
+
     <!--This page plugins -->
     <script src="assets/extra-libs/datatables.net/js/jquery.dataTables.min.js"></script>
     <script src="dist/js/pages/datatable/datatable-basic.init.js"></script>
