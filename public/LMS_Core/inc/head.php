ceb15fdc54332d9e7765f85aa78e2e562e8fe114
MartMbithi <martdevelopers254@gmail.com>
2021-02-28 10:12:00 +0300

    A Little Codebase House Keeping

--- public/LMS_Core/inc/head.php
+++ public/LMS_Core/inc/head.php
@@ -1,54 +1,52 @@
 <head>
-		<meta charset="utf-8">
-		<meta http-equiv="X-UA-Compatible" content="IE=edge">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		 <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
 
-		<title>LMS- A  Multi Purpose E Learning Platform</title>
+  <title>LMS- A Multi Purpose E Learning Platform</title>
 
-		<!-- Google font -->
-		<link href="https://fonts.googleapis.com/css?family=Lato:700%7CMontserrat:400,600" rel="stylesheet">
+  <!-- Google font -->
+  <link href="https://fonts.googleapis.com/css?family=Lato:700%7CMontserrat:400,600" rel="stylesheet">
 
-		<!-- Bootstrap -->
-		<link type="text/css" rel="stylesheet" href="css/bootstrap.min.css"/>
+  <!-- Bootstrap -->
+  <link type="text/css" rel="stylesheet" href="css/bootstrap.min.css" />
 
-		<!-- Font Awesome Icon -->
-		<link rel="stylesheet" href="css/font-awesome.min.css">
-		<script src="admin/dist/js/swal.js"></script>
+  <!-- Font Awesome Icon -->
+  <link rel="stylesheet" href="css/font-awesome.min.css">
+  <script src="admin/dist/js/swal.js"></script>
 
-		<!-- Custom stlylesheet -->
-		<link type="text/css" rel="stylesheet" href="css/style.css"/>
+  <!-- Custom stlylesheet -->
+  <link type="text/css" rel="stylesheet" href="css/style.css" />
 
-		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-		<!--[if lt IE 9]>
+  <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+  <!--[if lt IE 9]>
 		  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
 		  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 		<![endif]-->
 
-    </head>
+</head>
 
-    <!--Inject SWAL-->
-    <?php if(isset($success)) {?>
-    <!--This code for injecting an alert-->
-            <script>
-                        setTimeout(function ()
-                        {
-                            swal("Success","<?php echo $success;?>","success");
-                        },
-                            100);
-            </script>
+<!--Inject SWAL-->
+<?php if (isset($success)) { ?>
+  <!--This code for injecting an alert-->
+  <script>
+    setTimeout(function() {
+        swal("Success", "<?php echo $success; ?>", "success");
+      },
+      100);
+  </script>
 
-    <?php } ?>
+<?php } ?>
 
-    <?php if(isset($err)) {?>
-    <!--This code for injecting an alert-->
-            <script>
-                        setTimeout(function ()
-                        {
-                            swal("Failed","<?php echo $err;?>","error");
-                        },
-                            100);
-            </script>
+<?php if (isset($err)) { ?>
+  <!--This code for injecting an alert-->
+  <script>
+    setTimeout(function() {
+        swal("Failed", "<?php echo $err; ?>", "error");
+      },
+      100);
+  </script>
 
-    <?php } ?>
+<?php } ?>
