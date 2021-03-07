ab334d6cc9108aeea64af8b07ae8f8f0e1887833
MartMbithi <martdevelopers254@gmail.com>
2021-03-07 12:31:07 +0300

    Add Questions Bank Backend Logic

--- views/questions_bank.php
+++ views/questions_bank.php
@@ -4,6 +4,65 @@ include('../config/config.php');
 include('../config/checklogin.php');
 admin();
 require_once('../config/codeGen.php');
+/* Add Questions */
+if (isset($_POST['add_unit'])) {
+    //Error Handling and prevention of posting double entries
+    $error = 0;
+
+    if (isset($_POST['cc_id']) && !empty($_POST['cc_id'])) {
+        $cc_id = mysqli_real_escape_string($mysqli, trim($_POST['cc_id']));
+    } else {
+        $error = 1;
+        $err = "Course ID Cannot Be Empty";
+    }
+    if (isset($_POST['c_code']) && !empty($_POST['c_code'])) {
+        $c_code = mysqli_real_escape_string($mysqli, trim($_POST['c_code']));
+    } else {
+        $error = 1;
+        $err = "Code Cannot Be Empty";
+    }
+    if (isset($_POST['c_name']) && !empty($_POST['c_name'])) {
+        $c_name = mysqli_real_escape_string($mysqli, trim($_POST['c_name']));
+    } else {
+        $error = 1;
+        $err = "Name Cannot Be Empty";
+    }
+    if (isset($_POST['c_category']) && !empty($_POST['c_category'])) {
+        $c_category = mysqli_real_escape_string($mysqli, trim($_POST['c_category']));
+    } else {
+        $error = 1;
+        $err = "Course Name Cannot Be Empty";
+    }
+
+    if (isset($_POST['c_desc']) && !empty($_POST['c_desc'])) {
+        $c_desc = mysqli_real_escape_string($mysqli, trim($_POST['c_desc']));
+    } else {
+        $error = 1;
+        $err = "Description Cannot Be Empty";
+    }
+
+    if (!$error) {
+        //prevent Double entries
+        $sql = "SELECT * FROM  lms_course WHERE  c_code='$c_code'  ";
+        $res = mysqli_query($mysqli, $sql);
+        if (mysqli_num_rows($res) > 0) {
+            $row = mysqli_fetch_assoc($res);
+            if ($c_code == $row['c_code']) {
+                $err =  "A Unit With $c_code Exists";
+            }
+        } else {
+            $query = "INSERT INTO lms_course (cc_id, c_name, c_code, c_category, c_desc) VALUES (?,?,?,?,?)";
+            $stmt = $mysqli->prepare($query);
+            $rc = $stmt->bind_param('sssss', $cc_id, $c_name, $c_code, $c_category, $c_desc);
+            $stmt->execute();
+            if ($stmt) {
+                $success = "Added" && header("refresh:1; url=units.php");
+            } else {
+                $info = "Please Try Again Or Try Later";
+            }
+        }
+    }
+}
 require_once('../partials/head.php');
 ?>
 
@@ -71,12 +130,12 @@ require_once('../partials/head.php');
                                                 <td><?php echo $units->i_number; ?></td>
                                                 <td><?php echo $units->i_name; ?></td>
                                                 <td>
-                                                    <a class="badge badge-warning" data-toggle="modal" href="#<?php echo $units->ua_id; ?>">
+                                                    <a class="badge badge-warning" data-toggle="modal" href="#add-<?php echo $units->ua_id; ?>">
                                                         <i class="fas fa-external-link-alt"></i>
                                                         Create Question Bank
                                                     </a>
                                                     <!-- Add Question Bank Modal -->
-                                                    <div class="modal fade" id="<?php echo $units->ua_id; ?>">
+                                                    <div class="modal fade" id="add-<?php echo $units->ua_id; ?>">
                                                         <div class="modal-dialog  modal-lg">
                                                             <div class="modal-content">
                                                                 <div class="modal-header">
@@ -93,8 +152,11 @@ require_once('../partials/head.php');
                                                                             <div class="form-group col-md-6">
                                                                                 <label for="exampleInputEmail1">Unit Name</label>
                                                                                 <input type="text" name="c_name" value="<?php echo $units->c_name; ?>" readonly required class="form-control">
+                                                                                <!-- Hidden Values -->
+                                                                                <input type="hidden" name="c_id" value="<?php echo $units->c_id; ?>" readonly required class="form-control">
+                                                                                <input type="hidden" name="cc_id" value="<?php echo $units->cc_id; ?>" readonly required class="form-control">
+                                                                                <input type="hidden" name="i_id" value="<?php echo $units->i_id; ?>" readonly required class="form-control">
                                                                             </div>
-
                                                                             <div class="form-group col-md-6">
                                                                                 <label for="exampleInputEmail1">Unit Code</label>
                                                                                 <input type="text" name="c_code" readonly value="<?php echo $units->c_code; ?>" required class="form-control">
@@ -108,7 +170,7 @@ require_once('../partials/head.php');
                                                                         <div class="row">
                                                                             <div class="form-group col-md-12">
                                                                                 <label for="exampleInputEmail1">Questions</label>
-                                                                                <textarea type="text" name="q_details" class="form-control" id="<?php echo $units->ua_id; ?>" a></textarea>
+                                                                                <textarea type="text" name="q_details" class="form-control" id="<?php echo $units->ua_id; ?>"></textarea>
                                                                             </div>
                                                                         </div>
                                                                         <hr>
@@ -125,11 +187,12 @@ require_once('../partials/head.php');
                                                     </div>
                                                     <!-- End Modal -->
                                                 </td>
-                                                <!-- CK Editor -->
-                                                <script>
-                                                    CKEDITOR.replace('<?php echo $units->ua_id; ?>');
-                                                </script>
                                             </tr>
+                                            <!-- CK Editor -->
+
+                                            <script>
+                                                CKEDITOR.replace('<?php echo $units->ua_id; ?>');
+                                            </script>
                                         <?php } ?>
 
                                     </tbody>
