2d18e74bc3e2b8568239b5f0c86cd5c87a2c8283
MartMbithi <martdevelopers254@gmail.com>
2021-03-15 10:05:07 +0300

    Persistent System Settings On Meta Tags

--- partials/sidebar.php
+++ partials/sidebar.php
@@ -1,8 +1,8 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="dashboard.php" class="brand-link">
-        <img src="../public/sys_data/logo/logo.png" alt="Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-        <span class="brand-text font-weight-light">Integrated LMS</span>
+        <img src="../public/sys_data/logo/<?php echo $sys->sys_logo;?>" alt="Logo" class="brand-image  elevation-3" style="opacity: .8">
+        <span class="brand-text font-weight-light"><?php echo $sys->sys_name;?></span>
     </a>
 
     <!-- Sidebar -->
